41f3c411beff5017c677e53291464edd51cdd4b9
CHUMTS <humancorrector@gmail.com>
2023-05-21 21:24:23 +0300

    v2

--- src/main/java/com/chumts/task/dao/UserDao.java
+++ src/main/java/com/chumts/task/dao/UserDao.java
@@ -2,8 +2,9 @@ package com.chumts.task.dao;
 
 import com.chumts.task.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserDao extends JpaRepository<User, Long> {
 
 }
